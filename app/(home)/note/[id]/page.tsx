93033f2c5db43ec7525b368a1309b54099af6bb9
nrmnqdds <quddusnuriman@gmail.com>
2024-06-03 20:02:04 +0800

    :lipstick: formatting

--- app/(home)/note/[id]/page.tsx
+++ app/(home)/note/[id]/page.tsx
@@ -1,47 +1,47 @@
 import { constant } from "@/constants";
 import {
-	HydrationBoundary,
-	QueryClient,
-	dehydrate,
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
 } from "@tanstack/react-query";
 import type { Metadata } from "next";
 import { Suspense } from "react";
 import DynamicNoteContent from "./dynamic-note-content";
 
 type Props = {
-	params: { id: string };
+  params: { id: string };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-	const id = params.id;
+  const id = params.id;
 
-	const res = await fetch(`${constant.BACKEND_URL}/note/${id}`);
-	const data = await res.json();
+  const res = await fetch(`${constant.BACKEND_URL}/note/${id}`);
+  const data = await res.json();
 
-	return {
-		title: `StudyJom | ${data.data.title}`,
-	};
+  return {
+    title: `StudyJom | ${data.data.title}`,
+  };
 }
 
 const DynamicNotePage = async ({ params }: Props) => {
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	await queryClient.prefetchQuery({
-		queryKey: ["posts"],
-		queryFn: async () => {
-			const res = await fetch(`${constant.BACKEND_URL}/notes/${params.id}`);
-			const json = await res.json();
-			return json.data;
-		},
-	});
+  await queryClient.prefetchQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const res = await fetch(`${constant.BACKEND_URL}/notes/${params.id}`);
+      const json = await res.json();
+      return json.data;
+    },
+  });
 
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<HydrationBoundary state={dehydrate(queryClient)}>
-				<DynamicNoteContent id={params.id} />
-			</HydrationBoundary>
-		</Suspense>
-	);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <DynamicNoteContent id={params.id} />
+      </HydrationBoundary>
+    </Suspense>
+  );
 };
 
 export default DynamicNotePage;
