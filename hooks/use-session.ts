f90d5c1d29d467d1fb3f87a1230efdb2e494614d
nrmnqdds <quddusnuriman@gmail.com>
2024-06-05 02:20:55 +0800

    :goal_net: better error handling

--- hooks/use-session.ts
+++ hooks/use-session.ts
@@ -5,6 +5,9 @@ export const useSession = () => {
 		queryKey: ["session"],
 		queryFn: async () => {
 			const res = await fetch("/api/auth/session");
+			if (!res.ok) {
+				return null;
+			}
 			const json = await res.json();
 			return json.data;
 		},
