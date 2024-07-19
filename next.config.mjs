061a803c0d7339fe38a3697c15958d41ab69797b
nrmnqdds <quddusnuriman@gmail.com>
2024-07-19 10:50:34 +0800

    :wrench: change output target

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "standalone",
-	swcMinify: true,
+	// Uncomment if deploy using Docker
+	// output: "standalone",
+	// swcMinify: true,
 
 	webpack: (config) => {
 		config.module.rules.push({
